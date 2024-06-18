d9abef1f988056982cb7eb0edd0c794446a5b8b7
plasmaclouds <ales@vojtek.cloud>
2024-06-18 21:04:08 +0200

    Created  BigInteger PerformHeavyComputation with 1000 cycles

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
+using System.Numerics;
 
 namespace Azure_Web_App_Test.Pages
 {
@@ -8,7 +9,22 @@ namespace Azure_Web_App_Test.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        private static BigInteger PerformHeavyComputation()
+        {
+            int n = 1000; // number of iterations
+            BigInteger a = 0;
+            BigInteger b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                BigInteger temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+
         public int RandomNumber { get; private set; }
+        public BigInteger ComputationResult { get; private set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -18,6 +34,7 @@ namespace Azure_Web_App_Test.Pages
         public void OnGet()
         {
             RandomNumber = new Random().Next(1, 100); //generates a random number between 1 and 100
+            ComputationResult = PerformHeavyComputation(); // Perform heavy computation
         }
     }
 }
